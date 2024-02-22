ea86969cb752a9def5f4af38c6947d32b75acbe8
Ilya Ovsenev <ovsenev.ilya@yandex.ru>
2024-02-22 09:54:07 +0300

    qwer

---
+++ Static/JS/index.js
@@ -0,0 +1,9 @@
+﻿let ageUser = prompt('введите ваш возраст: ');
+if (ageUser < 18) {
+    alert('наши трансляции не предназначены для людей младше 18 лет. Вы будете перенаправлены.');
+    window.location.href = "http://www.google.com";
+}
+else {
+    let dateNow = new Date().toLocaleString();
+    alert('приветствуем вам на Life Spot ' + dateNow);
+}
